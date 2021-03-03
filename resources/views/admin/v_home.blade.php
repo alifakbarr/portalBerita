80bc4be951cb33d249aae236e6c9cf22f08d7b88
alifakbarr <alifakbarr8800@gmail.com>
2021-03-03 19:11:40 +0700

    mau ganti template

--- resources/views/admin/v_home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('admin.v_template')
-@section('title','Dashboard Admin')
-@section('content')
-    <p>Alif AKbar</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Admin
-Route::get('/admin',[AdminController::class,'index']);
